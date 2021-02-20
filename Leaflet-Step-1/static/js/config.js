e76b1fa47bbdb62757263b0d6d0093b162a82388
Sooyeon  Kim <71349348+SaraKim-sy@users.noreply.github.com>
2021-02-20 17:43:45 -0500

    progress

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2Jsb29tIiwiYSI6ImNra2VuM2FiYzAxanUydXA5cmpiNWU0N3kifQ.OSOICNvpQcmLkJBiEqC3aA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,52 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [41.1400, -104.8202],
+  zoom: 5
+});
+
+// Gray Mapbox background
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+
+// function for choosing the color based on earthquake magnitude
+function selectColor(magnitude) {
+  if (magnitude >= 5) {
+    return "#FF0000";
+  }
+  else if (magnitude >= 4) {
+    return "#FF4600";
+  }
+  else if (magnitude >= 3) {
+    return "#FF7B00";
+  }
+  else if (magnitude >= 2) {
+    return "#FFAF00";
+  }
+  else if (magnitude >= 1) {
+    return "#F7FF00";
+  }
+  else {
+    return "C2FF00";
+  }
+}
+
+// function for determining circle size based on earthquake magnitude
+function circleSize(magnitude) {
+  return magnitude * 5;
+}
+
+// Grabbing the GeoJSON data with d3
+d3.json(link, function(data) {
+
+  console.log(data)
+
+  
+
+})
