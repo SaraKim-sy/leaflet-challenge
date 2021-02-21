337821a8ac3ed5e93b118de18b5740ae1ed7d81e
Sooyeon  Kim <71349348+SaraKim-sy@users.noreply.github.com>
2021-02-20 19:25:22 -0500

    minor update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,7 +4,7 @@ var myMap = L.map("map", {
   zoom: 5
 });
 
-// Gray Mapbox background
+// Adding tile layer to the map
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
   maxZoom: 18,
